fdfcc3ee09cefafe1ab6fdd169b21fc5ce34c640
nguyenquangdo2002 <nguyenquangdodmx2002@gmail.com>
2025-09-09 23:58:27 +0700

    update

--- client/src/pages/admin-view/products.jsx
+++ client/src/pages/admin-view/products.jsx
@@ -1,18 +1,58 @@
 import { Button } from '@/components/ui/button'
+import {
+    Sheet,
+    SheetContent,
+
+    SheetHeader,
+    SheetTitle
+
+} from "@/components/ui/sheet"
 import React, { Fragment, useState } from 'react'
 
 function AdminProducts() {
     const [products, setProducts] = useState([])
+
+    const [openCreateProductsDialog, setOpenCreateProductsDialog] = useState(false);
+
     return (
         <Fragment>
             <div className='mb-5 w-full flex justify-end'>
 
 
-                <Button>Add New Product</Button>
+                <Button onClick={() => setOpenCreateProductsDialog(true)}>Add New Product</Button>
             </div>
-            <div>
-                this is product page
+            <div className='grid gap-4 md:grid-cols-3 lg:grid-cols-4'>
+                <div>
+                    this is product page
+                </div>
+                <div>
+                    this is product page
+                </div>
+                <div>
+                    this is product page
+                </div>
+                <div>
+                    this is product page
+                </div>
+                <div>
+                    this is product page
+                </div>
+                <div>
+                    this is product page
+                </div>
+                <div>
+                    this is product page
+                </div>
+
             </div>
+            <Sheet open={openCreateProductsDialog} onOpenChange={() => { setOpenCreateProductsDialog(false) }}>
+                <SheetContent side="right" >
+                    <SheetHeader>
+                        <SheetTitle>Add New Products</SheetTitle>
+                    </SheetHeader>
+
+                </SheetContent>
+            </Sheet>
         </Fragment>
     )
 }
